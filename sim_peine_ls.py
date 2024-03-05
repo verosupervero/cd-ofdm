a3bd30e944fda92ce08b56f6589a689900af6901
verosupervero <verobustamante95@gmail.com>
2024-03-05 15:55:38 -0300

    trabajamos sobre peine

--- sim_peine_ls.py
+++ sim_peine_ls.py
@@ -15,12 +15,15 @@ sz = lambda x: (np.size(x,0), np.size(x,1))
 
 N = 128 # numero de subportadoras
 pilot_period = 8 # un piloto cada esta cantidad de simbolos
-QAM_symb_len = N*500 # cantidad de simbolos QAM a transmitir
+cant_pilotos = N // pilot_period #lastima que tengo que poner esto aca y en la funcion
+
+QAM_symb_len = (N-cant_pilotos)*20 # cantidad de simbolos QAM a transmitir
 CP = N // 4 # prefijo ciclico
-SNRdB = 20 #dB
+SNRdB = 30 #dB
 
 # Para siempre generar los mimsos numeros aleatorios y tener repetibilidad
-#np.random.seed(123)
+np.random.seed(123)
+
 
 #  Simbolos
 Nbits = QAM_symb_len*qam.QAM_bits_per_symbol
@@ -30,26 +33,14 @@ data_bits = np.random.randint(2,size=Nbits)
 data_qam = qam.bits_to_qam(data_bits)
 
 # Conversion serie a paralelo
-data_par = data_qam.reshape(N, -1)
+data_par = data_qam.reshape(N-cant_pilotos, -1)
 
-# Agrego pilotos (en la frecuencia, todos unos)
+# Agrego pilotos
+all_symb, peine = utils.add_comb_pilots(data_par, qam.QAM(0), pilot_period,cant_pilotos)
+
+
 pilot_amplitude = qam.QAM(0)
-pilot_symbol =pilot_amplitude* np.ones(N, dtype=data_par.dtype)
-
-N_ODFM_sym = np.size(data_par,axis=1)
-N_pilots = (N_ODFM_sym // (pilot_period-1))+1
-
-# En esta matriz van los simbolos mas los pilotos
-all_symb = np.zeros((N,N_ODFM_sym+N_pilots),dtype=data_par.dtype)
-
-data_idx=0
-for symb_idx in range(0,all_symb.shape[1]):
-    # Si es un multiplo de pilot_period, mando un piloto
-    if symb_idx%pilot_period == 0:
-        all_symb[:,symb_idx] = pilot_symbol
-    else:  
-        all_symb[:,symb_idx] = data_par[:,data_idx]
-        data_idx = data_idx+1
+pilot_symbol=pilot_amplitude* np.ones(cant_pilotos, dtype=data_par.dtype)
 
 #%% Convierto a ODFM
 import ofdm
@@ -69,37 +60,40 @@ rx_symb = np.zeros(all_symb.shape, dtype=all_symb.dtype)
 
 for idx in range(0,rx_symb.shape[1]):
     # Vario levemente el canal (canal variante en el tiempo AR-1)
+    #x = np.zeros(128)
+   # x[0] = 1
+    #H = np.fft.fft(x)
     H = 0.998*H + 0.002*channels.fadding_channel(N)
     rx_ofdm= utils.add_noise(all_symb[:,idx]*H,SNRdB)
     #ofdm_noise = math.sqrt(var_noise)*np.random.standard_normal(size=N)
     #rx_ofdm = all_symb[:,idx]*H + ofdm_noise
     rx_symb[:,idx] = ofdm.mod(rx_ofdm)
 
+
 #%% Recepcion
-Nport = np.size(rx_symb, axis=0)
-N_rx = np.size(rx_symb, axis=1)
-Npilots_rx = (np.size(rx_symb, axis=1)//8) +1
-Ndata_rx = N_rx-Npilots_rx
+Nport = np.size(rx_symb, axis=0) #son 128
+N_rx = np.size(rx_symb, axis=1) # son 20
+Ndata_rx = N_rx
 
 # Prealoco filtro inverso y lo pongo plano
 Hinv = np.zeros((Nport,1), dtype=rx_symb.dtype)
 # Prealoco matriz para los simbolos ofdm recuperados
-rx_fix_symb = np.zeros((Nport,Ndata_rx), dtype=rx_symb.dtype)
+rx_fix_symb = np.zeros((Nport-cant_pilotos,Ndata_rx), dtype=rx_symb.dtype)
 
+##Demodulo los simbolos recibidos
 rx_freq = ofdm.demod(rx_symb)
 
-d_idx = 0
 for idx in range(0,N_rx):
     # Si es un multiplo de pilot_period, es un piloto
-    if idx%pilot_period == 0:
-        #Estimacion canal LS, y lo invierto
-        Hinv=np.divide(pilot_symbol, rx_freq[:,idx]) 
-    else:
-        rx_fix_symb[:,d_idx] = Hinv * rx_freq[:,idx]
-        d_idx = d_idx +1
-
+    indices = np.arange(0, N,pilot_period) 
+    pilots_rx_data = rx_freq[np.isin(np.arange(0,N),indices),idx]
+    Hinv= utils.estimate_channel(pilots_rx_data,pilot_symbol,N)
+    all_carriers = Hinv * rx_freq[:,idx]
+    rx_fix_symb[:,idx] = all_carriers[np.isin(np.arange(0,N),indices) == False]
+    
 # obtengo bits
 rx_bits = qam.qam_to_bits(rx_fix_symb.reshape(-1))
+
 
 # Calculo errores
 Nerr = np.sum(rx_bits != data_bits)
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ Created on Mon Mar  4 22:24:26 2024
 @author: Vero
 """
 import numpy as np
+import scipy
 
 def R(x:np.array, y:np.array=None) -> np.array:
     """
@@ -110,3 +111,71 @@ def add_comb_pilots(M: np.array, amplitude, period) -> np.array:
             data_idx = data_idx+1
     
     return all_symb, peine
+
+def add_noise(signal, SNR_dB):
+    # Calcular la potencia de la señal transmitida
+
+    if SNR_dB == -1:
+        return signal
+
+    signal_power = np.mean(np.abs(signal) ** 2)
+
+    # Calcular la potencia del ruido a partir de la relación señal a ruido (SNR)
+    noise_power = signal_power / (10 ** (SNR_dB / 10))
+
+    # Generar ruido gaussiano con la potencia calculada
+    noise = np.random.normal(0, np.sqrt(noise_power), len(signal))
+
+    # Agregar ruido a la señal transmitida
+    noise_signal = signal + noise
+
+    return noise_signal
+
+def add_comb_pilots(M: np.array, amplitude,period, N_pilots) -> np.array:
+    """
+    Estima el H por LS y el simbolo recibido
+
+    Parameters
+    ----------
+    M : np.array
+        DESCRIPTION.
+    amplitude : TYPE
+        DESCRIPTION.
+    period : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    all_symb : TYPE
+        DESCRIPTION.
+
+    """
+    N = M.shape[0] #Cantidad subportadoras
+    # En esta matriz van los simbolos mas los pilotos
+    all_symb = np.zeros((N+N_pilots,M.shape[1]),dtype=M.dtype)
+    peine = np.zeros((N+N_pilots,1),dtype=M.dtype)
+    data_idx=0
+    for f_idx in range(0,N+N_pilots):
+        if f_idx%period == 0:
+            # Si es un multiplo de pilot_period, mando un piloto
+            all_symb[f_idx,:] = amplitude
+            peine[f_idx] = amplitude # para devolver una muestra del peine
+        else:  
+            # Si no, copio la fila
+            all_symb[f_idx,:] = M[data_idx,:]
+            data_idx = data_idx+1
+    
+    return all_symb, peine
+
+def estimate_channel(received_pilots, pilot_symbols, N_portadoras, interpolation_method = 'linear'):
+  
+    H_est=np.divide(received_pilots, pilot_symbols) 
+
+    if interpolation_method == 'spline':
+        f= scipy.interpolate.interp1d(np.arange(0,len(H_est)), H_est, kind='cubic')  
+    else:
+        f= scipy.interpolate.interp1d(np.arange(0,len(H_est)), H_est, kind=interpolation_method)
+
+    # Interpola los valores de H para las distintas portadoras.
+    interpolated_H = f(np.linspace(0, len(H_est)-1, N_portadoras))
+    return interpolated_H
