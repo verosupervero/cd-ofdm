b73589777be1cebf484f1283dde7a430308b25b5
verosupervero <verobustamante95@gmail.com>
2024-03-06 01:05:23 -0300

    sim2_blk: Agrego el ruido a la señal en el tiempo en lugar de a los simbolos. Emprolijo pilotos. BER

--- sim2_blk_ls_variante.py
+++ sim2_blk_ls_variante.py
@@ -13,42 +13,30 @@ import utils
 
 sz = lambda x: (np.size(x,0), np.size(x,1))
 
-N = 128 # numero de subportadoras
+N_subportadoras = 128 # numero de subportadoras
 pilot_period = 8 # un piloto cada esta cantidad de simbolos
 N_OFDM_sym = 500
-QAM_symb_len = N*N_OFDM_sym # cantidad de simbolos QAM a transmitir
+QAM_symb_len = N_subportadoras*N_OFDM_sym # cantidad de simbolos QAM a transmitir
 SNRdB = 20 #dB
 
 # Para siempre generar los mimsos numeros aleatorios y tener repetibilidad
-#np.random.seed(123)
+np.random.seed(12345)
 
 #  Simbolos
-Nbits = QAM_symb_len*qam.QAM_bits_per_symbol
+Nbits = QAM_symb_len*qam.QAM_bits_per_symbol #cantidad de bits a generar
 data_bits = np.random.randint(2,size=Nbits)
 
 # Convierto la serie de bits a una serie de simbolos qam
 data_qam = qam.bits_to_qam(data_bits)
 
 # Conversion serie a paralelo
-data_par = data_qam.reshape(N, -1)
+data_par = data_qam.reshape(N_subportadoras, -1)
 
 # Agrego pilotos (en la frecuencia, todos unos)
 pilot_amplitude = qam.QAM(0)
-pilot_symbol =pilot_amplitude* np.ones(N, dtype=data_par.dtype)
 
-N_pilots = N_OFDM_sym // pilot_period
-
-# En esta matriz van los simbolos mas los pilotos
-all_symb = np.zeros((N,N_OFDM_sym+N_pilots),dtype=data_par.dtype)
-
-data_idx=0
-for symb_idx in range(0,all_symb.shape[1]):
-    # Si es un multiplo de pilot_period, mando un piloto
-    if symb_idx%pilot_period == 0:
-        all_symb[:,symb_idx] = pilot_symbol
-    else:  
-        all_symb[:,symb_idx] = data_par[:,data_idx]
-        data_idx = data_idx+1
+# Agrego pilotos (en la frecuencia, todos unos)
+all_symb, pilot_symbol = utils.add_block_pilots(data_par, amplitude=pilot_amplitude, period=pilot_period)
 
 #%% Convierto a ODFM
 import ofdm
@@ -56,28 +44,22 @@ import ofdm
 #tx_pilot = ofdm.mod(all_symb[:,0])
 
 #%% Canal
-H = channels.fadding_channel(N) #Canal en frecuencia
+H = channels.fadding_channel(N_subportadoras) #Canal en frecuencia
 # Prealoco matriz con simbolos recibidos
 rx_symb = np.zeros(all_symb.shape, dtype=all_symb.dtype)
 
-# Calculo de la varianza del ruido
-# SNR = 10.log(P_S/P_N)
-# 10^(SNR/10) = var(symb_QAM)/var(N)
-# var(N) = var(symb_QAM)/10^(SNR/10)
-#var_noise = qam.eps/pow(10, SNR/10)
-
 for idx in range(0,rx_symb.shape[1]):
     # Vario levemente el canal (canal variante en el tiempo AR-1)
-    H = 0.998*H + 0.002*channels.fadding_channel(N)
-    rx_ofdm= utils.add_noise(all_symb[:,idx]*H,SNRdB)
+    H = 0.998*H + 0.002*channels.fadding_channel(N_subportadoras)
+    rx_ofdm = all_symb[:,idx]*H
     #ofdm_noise = math.sqrt(var_noise)*np.random.standard_normal(size=N)
     #rx_ofdm = all_symb[:,idx]*H + ofdm_noise
-    rx_symb[:,idx] = ofdm.mod(rx_ofdm)
+    rx_symb[:,idx],noise_power = utils.add_noise(ofdm.mod(rx_ofdm),SNRdB)
 
 #%% Recepcion
 Nport = np.size(rx_symb, axis=0)
 N_rx = np.size(rx_symb, axis=1)
-Npilots_rx = (np.size(rx_symb, axis=1)//8) +1
+Npilots_rx, idx_pilots_rx = utils.recover_Npilots_blk(N_rx, pilot_period)
 Ndata_rx = N_rx-Npilots_rx
 
 # Prealoco filtro inverso y lo pongo plano
@@ -90,7 +72,7 @@ rx_freq = ofdm.demod(rx_symb)
 d_idx = 0
 for idx in range(0,N_rx):
     # Si es un multiplo de pilot_period, es un piloto
-    if idx%pilot_period == 0:
+    if idx in idx_pilots_rx:
         #Estimacion canal LS, y lo invierto
         Hinv=np.divide(pilot_symbol, rx_freq[:,idx]) 
     else:
@@ -104,7 +86,7 @@ rx_bits = qam.qam_to_bits(rx_fix_symb.reshape(-1))
 Nerr = np.sum(rx_bits != data_bits)
 Perr = Nerr / np.size(data_bits)
 
-print(f"""Prob errrores: {Perr*100}%""")
+print(f"""BER: {Perr:.1E}""")
 
 #%% graficos
 qam.plot_qam_constellation(rx_fix_symb)
